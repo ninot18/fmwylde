77a417557c76abed25c083b74d2f01f7b1fcd118
Pedro Rojas <pedrogiovanny18@gmail.com>
2024-08-13 12:56:31 +0200

    añadidas algunas páginas faltantes, incluido el blog

---
+++ src/content/config.ts
@@ -0,0 +1,19 @@
+import { defineCollection, reference, z } from "astro:content";
+
+ const blogCollection = defineCollection({
+    type: 'content',
+    schema: ({ image }) => z.object({
+        title: z.string(),
+        date: z.date(),
+        description: z.string(),
+        image: image().refine( img => img.width < 1200, {
+         message: 'Image should be lower than 1200px',
+        }),
+        author: z.string(),
+        tags: z.array(z.string()),
+    }),
+ });
+
+ export const collection = {
+    blog: blogCollection,
+ };
