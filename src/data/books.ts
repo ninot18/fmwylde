0c128139fa2c0634d635541232fdc20519e4a501
Pedro Rojas <pedrogiovanny18@gmail.com>
2024-08-21 22:26:05 +0200

    Añadidas las reviews!

---
+++ src/data/books.ts
@@ -0,0 +1,30 @@
+export const books = [
+  {
+    id: 'el-sr-de-los-anillos-1',
+    title: 'El Señor de los anillos (1/3)',
+    img: "../src/assets/images/la-comunidad-del-anillo.webp",
+    opinion: "Opinión de la comunidad del anillo.",
+    author: 'J. R. R. Tolkien',
+  },
+  {
+    id: 'antifragil',
+    title: 'Antifrágil',
+    img: "../src/assets/images/antifragil.webp",
+    opinion: 'Opinión de Antifrágil.',
+    author: 'Nassim Taleb'
+  },
+  {
+    id: 'meditaciones',
+    title: 'Meditaciones',
+    img: "../src/assets/images/meditaciones.webp",
+    opinion: 'Opinión de Meditaciones, de Marco Aurelio.',
+    author: 'Marco Aurelio',
+  },
+  {
+    id: 'el-guardian-de-espadas',
+    title: 'El guardián de espadas',
+    img: '../src/assets/images/elguardiandeespadas.webp',
+    opinion: 'Opinión de El Guardián de Espadas, de Cassandra Clare.',
+    author: 'Cassandra Clare',
+  },
+]
