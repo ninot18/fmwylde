dace680352a1aaa33e10c958c99783af4945aff4
Pedro Rojas <pedrogiovanny18@gmail.com>
2024-08-21 13:03:42 +0200

    Añadida sección del archivo, con posts

--- src/utils/post.ts
+++ src/utils/post.ts
@@ -6,8 +6,21 @@ export const getCategories = async () => {
 	return Array.from(categories)
 }
 
+export const getArchiveCategories = async () => {
+	const posts = await getCollection('archive')
+	const categories = new Set(posts.map((post) => post.data.category))
+	return Array.from(categories)
+}
+
 export const getPosts = async (max?: number) => {
 	return (await getCollection('blog'))
+		.filter((post) => !post.data.draft)
+		.sort((a, b) => b.data.pubDate.valueOf() - a.data.pubDate.valueOf())
+		.slice(0, max)
+}
+
+export const getArchivePosts = async (max?: number) => {
+	return (await getCollection('archive'))
 		.filter((post) => !post.data.draft)
 		.sort((a, b) => b.data.pubDate.valueOf() - a.data.pubDate.valueOf())
 		.slice(0, max)
